7f8fd60a8ed7b4ff5b8056f366162093164e5dd4
Lahess <80914959+LahessAimere@users.noreply.github.com>
2023-12-13 14:05:24 +0100

    Theorical Editor

---
+++ Assets/Scripts/Monobehavior/CustomComponent.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomComponent : MonoBehaviour
+{
+    [SerializeField, Scene] private int _scene;
+
+    [ContextMenu("Load Scene")]
+
+    private void LoadScene()
+    {
+        if (Application.isPlaying)
+        {
+            SceneManager.LoadScene(_scene.ToString());
+        }
+        else
+        {
+            string scenePath = SceneManager.GetSceneByBuildIndex(_scene).path;
+            EditorSceneManager.OpenScene(scenePath);
+        }
+    }
+}
